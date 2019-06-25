c1323e159e048c615bb2046191d7eeeac607839a
MindX <MindxSolutions@outlook.com>
2019-06-25 11:21:31 +0500

    initial commint

---
+++ Datingapp.API/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datingapp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datingapp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _Context;
+        public ValuesController(DataContext context)
+        {
+            _Context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValue()
+        {
+            
+            var data = await _Context.Values.ToListAsync();
+
+            return Ok(data);
+            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValues(int id)
+        {
+            var value  = await _Context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
